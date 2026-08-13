aa6de6292581a9b5d3c9a15a89ed75c7e922e6b8
BOT92 <Bastian_Beck11@web.de>
2026-08-13 13:42:09 +0200

    Refresh cache for v11.7.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="reppilot-v11-7-3";const ASSETS=["./","./index.html","./styles.css?v=11.7.3","./app.js?v=11.7.3","./workout-fix.js?v=11.7.3","./manifest.json","./icon-192.png","./icon-512.png","./stretch-anatomy-v11.7.2.png?v=11.7.3"];
+const CACHE="reppilot-v11-7-4";const ASSETS=["./","./index.html","./styles.css?v=11.7.4","./app.js?v=11.7.4","./workout-fix.js?v=11.7.4","./manifest.json","./icon-192.png","./icon-512.png","./stretch-anatomy-v11.7.2.png?v=11.7.4"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const x=r.clone();e.waitUntil(caches.open(CACHE).then(c=>c.put(e.request,x)))}return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match("./index.html"))))});
